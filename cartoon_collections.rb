38d15f2db68fcde86aa750f89bb46055cdfd9f3d
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-07 18:49:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,10 +13,10 @@ end
 
 def long_planeteer_calls(array)
   array.each { |word|
-    if word.length >= 4
-      return true
-    else word.length <= 4
+    if word.length < 4
       return false
+    else
+      return true
     end
   }
 end
