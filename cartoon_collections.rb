6c350cc20cad6ce8152b7ab4d489411412dc376c
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-07 18:44:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,14 @@ def summon_captain_planet(array)
   }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each { |word|
+    if word.length > 4
+      true
+    else
+      false
+    end
+  }
 end
 
 def find_the_cheese# code an argument here
