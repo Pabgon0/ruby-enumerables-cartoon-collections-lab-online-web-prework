7961bb949cae65d18a8ed62b8b3b18358c784a77
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-07 19:19:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include?("cheddar", "gouda", "camembert")
+  array.include?("cheddar"|| "gouda"|| "camembert")
   if true
     print "#{word}"
   else
