6bb08187840d9298a4a5035a2bf97496dfa47f4b
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-07 19:16:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,14 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  
+  array.include? { |word| word
+  return word
+  }
+  if true
+    print "#{word}"
+  else
+    return nil
+  end
   
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
